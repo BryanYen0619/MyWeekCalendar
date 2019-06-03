fa7f39475fc82b1b2c1c4a62ab98f81ea6ae39d2
Bryan.Yen <bryan.yen@yfyshop.com>
2019-06-03 10:15:50 +0800

    # 週選擇異常處理

--- weekcalendar/src/main/java/bryanyen/myweekcalendar/WeekCalendar.java
+++ weekcalendar/src/main/java/bryanyen/myweekcalendar/WeekCalendar.java
@@ -225,10 +225,12 @@ public class WeekCalendar extends LinearLayout {
 
     public void moveToPreviousWeek() {
         BusProvider.getInstance().post(new Event.SetCurrentPageEvent(-1));
+        BusProvider.getInstance().post(new Event.UpdateSelectedDateEvent(-7));
     }
 
     public void moveToNextWeek() {
         BusProvider.getInstance().post(new Event.SetCurrentPageEvent(1));
+        BusProvider.getInstance().post(new Event.UpdateSelectedDateEvent(7));
     }
 
     public void moveToPrevious() {
